a03e39dae559f266713ad5ccde506e7aaae82e40
Misha Onyshchenko <mishaonyshchenko@McPro.local>
2023-07-26 23:44:18 +0300

    bot added

---
+++ src/main/java/com/rr/botapi/bot/TelegramBot.java
@@ -0,0 +1,64 @@
+package com.rr.botapi.bot;
+
+import com.rr.botapi.config.BotConfig;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@AllArgsConstructor
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final BotConfig botConfig;
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    try {
+//                        currency = CurrencyService.getCurrencyRate(messageText, currencyModel);
+                    } catch (Exception e) {
+                        sendMessage(chatId, "We have not found it");
+                    }
+//                    sendMessage(chatId, currency);
+            }
+        }
+
+    }
+
+    private void startCommandReceived(Long chatId, String name) {
+        String answer = "Hi, " + name + ", enjoy the sun!";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(Long chatId, String textToSend) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+}
---
+++ src/main/java/com/rr/botapi/config/BotConfig.java
@@ -0,0 +1,13 @@
+package com.rr.botapi.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "bot")
+public class BotConfig {
+    String name;
+    String token;
+}
